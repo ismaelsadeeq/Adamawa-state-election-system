09ddcdb4577789dbadca475bb21f81afa3f16c3b
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-09-16 07:07:46 +0100

    election

--- controllers/election.controller.js
+++ controllers/election.controller.js
@@ -43,14 +43,7 @@ const createElection = async (req,res)=>{
   return res.json(responseData);
 }
 const getElection = async (req,res)=>{
-  const election = await models.election.findOne(
-    {
-      include:[
-        {
-          model:models.party
-        }
-      ]
-    }
+  const election = await models.election.findOne( 
   ); 
   if(!election.status ==true){
     responseData.message = "election not published";
@@ -65,6 +58,27 @@ const getElection = async (req,res)=>{
     return res.json(responseData);
   }
   responseData.message = "completed";
+  responseData.status = true;
+  responseData.data = election;
+  return res.json(responseData);
+}
+const deleteElection = async (req,res)=>{
+  const user = req.user
+  if(!user.isAdmin){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  const election = await models.election.destroy( 
+  ); 
+  if(!election){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  responseData.message = "election deleted";
   responseData.status = true;
   responseData.data = election;
   return res.json(responseData);
@@ -97,32 +111,33 @@ const getElectionDetailAdmin = async (req,res)=>{
   responseData.data = election;
   return res.json(responseData);
 }
-// const getElectionDetail = async (req,res)=>{
-//   const election = await models.election.findOne(); 
-//   if(election.status ==true){
-//     responseData.message = "election not published";
-//     responseData.status = false;
-//     responseData.data = undefined;
-//     return res.json(responseData);
-//   }
-//   const party = await models.party.findOne(
-//     {
-//       where:{
-//         electionId:election.id
-//       }
-//     }
-//   );
-//   if(!election){
-//     responseData.message = "something went wrong";
-//     responseData.status = false;
-//     responseData.data = undefined;
-//     return res.json(responseData);
-//   }
-//   responseData.message = "completed";
-//   responseData.status = true;
-//   responseData.data = election;
-//   return res.json(responseData);
-// }
+const getElectionDetail = async (req,res)=>{
+  const election = await models.election.findOne(
+    {
+      include:[
+        {
+          model:models.party
+        }
+      ]
+    }
+  ); 
+  if(!election.status ==true){
+    responseData.message = "election not published";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  if(!election){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  responseData.message = "completed";
+  responseData.status = true;
+  responseData.data = election;
+  return res.json(responseData);
+}
 const publishElection = async (req,res)=>{
   const election = await models.election.update(
     {
@@ -220,13 +235,113 @@ const submitResult = async (req,res)=>{
   return res.json(responseData);
 }
 
+const createParty = async (req,res)=>{
+  const data = req.body;
+  if(!data){
+    responseData.message = "data is required";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  const partyExist = await models.party.findOne(
+    {
+      where:{
+        name:data.name
+      }
+    }
+  )
+  if(partyExist){
+    responseData.message = "party exist";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  const election = await models.election.findOne( 
+  ); 
+  if(typeof(data.contestantName)=="object"){
+    data.contestantName = JSON.stringify(data.contestantName)
+  }
+  const party = await models.party.create(
+    {
+      id:uuid.v4(),
+      electionId:election.id,
+      name:data.name,
+      contestantName:data.contestantName,
+      votes:"0"
+    }
+  );
+  if(!party){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  responseData.message = "party successfully created";
+  responseData.status = true;
+  responseData.data = party;
+  return res.json(responseData);
+}
+
+const editParty = async (req,res)=>{
+  const data = req.body;
+  const partyId = req.params.id
+  if(typeof(data.contestantName)=="object"){
+    data.contestantName = JSON.stringify(data.contestantName)
+  }
+  console.log(typeof(data.contestantName))
+  const party = await models.party.update(
+    {
+      name:data.name,
+      contestantName:data.contestantName
+    },
+    {
+      where:{
+        id:partyId
+      }
+    }
+  );
+  if(!party){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  responseData.message = "party successfully edited";
+  responseData.status = true;
+  responseData.data = party;
+  return res.json(responseData);
+}
+const deleteParty = async (req,res)=>{
+  const data = req.body;
+  const partyId = req.params.id
+  const party = await models.party.destroy(
+    {
+      where:{
+        id:partyId
+      }
+    }
+  );
+  if(!party){
+    responseData.message = "something went wrong";
+    responseData.status = false;
+    responseData.data = undefined;
+    return res.json(responseData);
+  }
+  responseData.message = "party successfully deleted";
+  responseData.status = true;
+  responseData.data = party;
+  return res.json(responseData);
+}
 
 module.exports = {
   createElection,
   getElection,
   getElectionDetailAdmin,
-  // getElectionDetail,
+  getElectionDetail,
   publishElection,
-  submitResult
-
+  submitResult,
+  createParty,
+  editParty,
+  deleteParty,
+  deleteElection
 }
--- routes/election.js
+++ routes/election.js
@@ -7,18 +7,25 @@ router.post('/create',
   passport.authenticate('jwt',{session:false}),
 	controller.createElection
 );
-router.get('/election/:id',
+router.post('/create/party',
   passport.authenticate('jwt',{session:false}),
-	controller.getElection
+	controller.createParty
 );
-router.get('/election-detail/admin',
+router.put('/edit/party/:id',
+  passport.authenticate('jwt',{session:false}),
+	controller.editParty
+);
+router.delete('/party/:id',
+  passport.authenticate('jwt',{session:false}),
+	controller.deleteParty
+);
+router.get('/admin',
   passport.authenticate('jwt',{session:false}),
 	controller.getElectionDetailAdmin
 );
-// router.get('/election-detail',
-//   passport.authenticate('jwt',{session:false}),
-// 	controller.getElectionDetail
-// );
+router.get('/election-detail',
+	controller.getElectionDetail
+);
 router.get('/publish/:id',
   passport.authenticate('jwt',{session:false}),
 	controller.publishElection
@@ -27,5 +34,11 @@ router.post('/submit/:id',
   passport.authenticate('jwt',{session:false}),
 	controller.submitResult
 );
-
+router.get('/',
+	controller.getElection
+);
+router.delete('/',
+  passport.authenticate('jwt',{session:false}),
+	controller.deleteElection
+);
 module.exports = router;

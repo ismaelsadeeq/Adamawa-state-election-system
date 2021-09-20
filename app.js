5eaab8d93117ee58e84a32fe52fe790c419d3a8f
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-09-20 13:58:08 +0100

    adding cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport');
-
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -17,6 +17,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
+app.options('*', cors()) // include before other routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
